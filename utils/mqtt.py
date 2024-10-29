dcd89194de4ff47c1772f272c541abf864597579
Adrian Losada <adrian.losada@laude.tech>
2024-10-29 16:39:04 +0100

    on_message threading for not blocking the sub

--- utils/mqtt.py
+++ utils/mqtt.py
@@ -1,30 +1,35 @@
 import paho.mqtt.client as mqtt
 import json, os
 
-def publish_mqtt(client, topic, **kwargs):
+from threading import Thread
+
+def publish_mqtt(client: mqtt.Client, topic, **kwargs):
     client_id = client._client_id.decode("utf-8")
     print(f"Node: {client_id} | Publishing on /{topic}", flush=True)
     client.publish(f"/{topic}", json.dumps(kwargs))
 
-def on_connect(client, userdata, flags, rc, properties):
+def on_connect(client: mqtt.Client, userdata, flags, rc, properties):
     if rc == 0:
         print("Connected successfully", flush=True)
-        client.subscribe(f"/{userdata}")
+        client.subscribe(f"/{userdata['topic']}")
     else:
         print("Connection failed with code", rc)
 
-def connect_mqtt(topic: str, on_message):
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, topic)
+def on_message(client, userdata, msg):
+    process_message = userdata['process_message']
+    Thread(target=process_message, args=(client, userdata["topic"], msg)).start()
+
+def connect_mqtt(topic: str, process_message = None):
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, userdata={'topic': topic, 'process_message': process_message})
     client.on_connect = on_connect
     client.on_message = on_message
-    client.user_data_set(topic)
-
+    
     broker = os.getenv('MQTT_BROKER', 'localhost')  # 'localhost' es el valor por defecto si la variable no está definida
     client.connect(broker)
     return client
 
-def connect_node(topic: str, on_message):
-    client = connect_mqtt(topic, on_message)
+def connect_node(topic: str, process_message):
+    client = connect_mqtt(topic, process_message)
     print() ## Necessary
     try:
         client.loop_forever()
