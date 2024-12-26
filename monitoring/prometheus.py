51b052da5a422fbc1d9fbc8fc8a0c2128c8b29da
Nodo4 <laude@example.es>
2024-12-26 09:51:30 +0000

    Fix monitoring

--- monitoring/prometheus.py
+++ monitoring/prometheus.py
@@ -14,7 +14,7 @@ active = 0
 
 def monitor(test_folder):
     test_path = join("database", "results", test_folder)
-    prometh = PrometheusConnect("http://192.168.10.194:9090")
+    prometh = PrometheusConnect("http://localhost:9090")
     conn = sqlite3.connect(join(test_path, "metrics.db"))
     cursor = conn.cursor()
 
